f23b21c838e50221218e3cbdea72b74aea2ac30b
yoonus <yoonustehrani28@gmail.com>
2024-06-04 04:16:18 +0330

    national airports search fixed

--- app/Http/Controllers/NationalAirportController.php
+++ app/Http/Controllers/NationalAirportController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Models\Airport;
+use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
 
 class NationalAirportController extends Controller
@@ -10,9 +11,30 @@ class NationalAirportController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index()
+    public function index(Request $request)
     {
-        return \DB::table('airports')->whereCountryCode('IR')->whereNotNull('city_name_fa')->groupBy('city_name_fa')->select('city_name_fa', 'city_name')->get();
+        $request->validate([
+            'q' => 'string|min:2'
+        ]);
+        $search_query = $request->query('q');
+        $cities = \DB::table('airports');
+        $cities->where('country_code', 'IR')->whereNotNull('city_name_fa');
+        if (preg_match('/^[A-Z]{2,4}$/', $search_query)) {
+            $cities->where('IATA_code', 'like', "$search_query%");
+        } else if (preg_match('/[A-Za-z]+/', $search_query)) {
+            $cities->where(function(Builder $query) use($search_query) {
+                $query->where('name', 'like', "%$search_query%");
+                $query->orWhere('city_name', 'like', "%$search_query%");
+            });
+        } else if (preg_match('/[^a-zA-Z0-9\_\@\!\/\$\#\^\&\*\(\)\-\+]{1,}/', $search_query)) {
+            $cities->where(function(Builder $query) use($search_query) {
+                $query->where('name_fa', 'like', "%$search_query%");
+                $query->orWhere('city_name_fa', 'like', "%$search_query%");
+            });
+        }
+        $cities->groupBy('IATA_code')->where('IATA_code', '!=', 'IKA')->select('city_name_fa', 'IATA_code');
+        return $cities->get();
+        //
     }
     /**
      * Display the specified resource.
