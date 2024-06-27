6c6b2b5fd15bf181490f3c45565075b06f5406a5
yoonus <yoonustehrani28@gmail.com>
2024-06-27 15:09:45 +0330

    sanctum problem

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,6 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
+        $middleware->validateCsrfTokens(except: [
+            'api/airports/*'
+        ]);
         $middleware->statefulApi();
         $middleware->trustProxies(at: '*');
             $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
