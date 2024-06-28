68024069ab51d76efa7ac6c0e4c5ee89877e7543
yoonus <yoonustehrani28@gmail.com>
2024-06-28 14:59:26 +0330

    national airport controller bug fix in search + rating

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,9 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        // $middleware->validateCsrfTokens(except: [
-        //     'api/login'
-        // ]);
         $middleware->statefulApi();
         $middleware->trustProxies(at: ['*']);
         $middleware->trustProxies(headers: Request::HEADER_X_FORWARDED_FOR |
