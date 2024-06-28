641d83a5b1ad944dfe2fc5304581ffaa55795a7c
yoonus <yoonustehrani28@gmail.com>
2024-06-28 17:44:39 +0330

    enabled request validation for flight search

--- app/Http/Controllers/FlightApiController.php
+++ app/Http/Controllers/FlightApiController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use App\Http\Requests\FlightSearchRequest;
 use App\Http\Resources\FlightSearchCollection;
 use App\Models\Airline;
-use Illuminate\Http\Request;
 use App\Parto\Domains\Flight\Enums\FlightCabinType;
 use App\Parto\Domains\Flight\Enums\FlightLocationType;
 use App\Parto\Domains\Flight\FlightOriginDestination;
@@ -15,7 +14,7 @@ use Illuminate\Support\Carbon;
 
 class FlightApiController extends Controller
 {
-    public function searchOneWay(Request $request)
+    public function searchOneWay(FlightSearchRequest $request)
     {
         /**
          * @var \App\Parto\PartoClient
