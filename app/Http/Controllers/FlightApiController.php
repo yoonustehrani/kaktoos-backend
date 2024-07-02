a133897964f4686f7991a942073f800dc378493e
yoonus <yoonustehrani28@gmail.com>
2024-07-02 18:15:32 +0330

    1. Refactored FlightApiController due to common code 2. Altered request class to be useful for roundtrip requests

--- app/Http/Controllers/FlightApiController.php
+++ app/Http/Controllers/FlightApiController.php
@@ -7,16 +7,18 @@ use App\Http\Resources\FlightSearchCollection;
 use App\Parto\Domains\Flight\Enums\FlightCabinType;
 use App\Parto\Domains\Flight\Enums\FlightLocationType;
 use App\Parto\Domains\Flight\FlightOriginDestination;
+use App\Parto\Domains\Flight\FlightSearch;
 use App\Traits\FlightsSideJobs;
 use App\Traits\PaginatesCollections;
+use Illuminate\Http\Request;
 use Illuminate\Support\Carbon;
 
 class FlightApiController extends Controller
 {
     use PaginatesCollections, FlightsSideJobs;
-
-    public function searchOneWay(FlightSearchRequest $request)
+    public function search(string $method, FlightSearchRequest $request)
     {
+        abort_if(! in_array($method, ['one-way', 'roundtrip']), 404, "Search method not found");
         [$originLocationType, $origin] = explode(':', $request->input('origin'));
         [$destinationLocationType, $destination] = explode(':', $request->input('destination'));
         /**
@@ -30,18 +32,44 @@ class FlightApiController extends Controller
                 infant: $request->input('passengers.infants', 0)
                 
             )
-            ->setCabinType(FlightCabinType::tryFrom($request->input('cabin_type', null)))
-            ->oneWay(new FlightOriginDestination(
-                originCode: $origin,
-                originLocationType: FlightLocationType::tryFrom($originLocationType),
-                destinationCode: $destination,
-                destinationLocationType: FlightLocationType::tryFrom($destinationLocationType),
-                departureDateTime: Carbon::createFromFormat('Y-m-d', $request->input('date'))
-            ));
-        
-        $cache_key = implode(".", [$request->input('origin') , $request->input('destination') , $request->input('date')]);
-        $flights = cache()->remember(md5($cache_key), config('services.parto.timing.flights_cache'), function () use($parto, $flight_search) {
-            return $parto->searchFlight($flight_search)?->PricedItineraries ?? [];
+            ->setCabinType(FlightCabinType::tryFrom($request->input('cabin_type', null)));
+        switch ($method) {
+            case 'one-way':
+                $flight_search->oneWay(new FlightOriginDestination(
+                    originCode: $origin,
+                    originLocationType: FlightLocationType::tryFrom($originLocationType),
+                    destinationCode: $destination,
+                    destinationLocationType: FlightLocationType::tryFrom($destinationLocationType),
+                    departureDateTime: Carbon::createFromFormat('Y-m-d', $request->input('date'))
+                ));
+                break;
+            case 'roundtrip':
+                $flight_search->roundtrip(
+                    first: new FlightOriginDestination(
+                        originCode: $origin,
+                        originLocationType: FlightLocationType::tryFrom($originLocationType),
+                        destinationCode: $destination,
+                        destinationLocationType: FlightLocationType::tryFrom($destinationLocationType),
+                        departureDateTime: Carbon::createFromFormat('Y-m-d', $request->input('date'))
+                    ),
+                    second: new FlightOriginDestination(
+                        originCode: $destination,
+                        originLocationType: FlightLocationType::tryFrom($destinationLocationType),
+                        destinationCode: $origin,
+                        destinationLocationType: FlightLocationType::tryFrom($originLocationType),
+                        departureDateTime: Carbon::createFromFormat('Y-m-d', $request->input('return_date'))
+                    )
+                );
+                break;
+        }
+        return $this->returnFlights($flight_search, $request);
+    }
+
+    protected function returnFlights(FlightSearch $flightSearch, Request $request)
+    {
+        $cache_key = implode(".", [$request->input('origin') , $request->input('destination') , $request->input('date'), $request->input('return_date')]);
+        $flights = cache()->remember(md5($cache_key), config('services.parto.timing.flights_cache'), function () use($flightSearch) {
+            return app('parto')->searchFlight($flightSearch)?->PricedItineraries ?? [];
         });
         if (count($flights) > 0) {
             $this->takeCareOfSideEffects(collect($flights), $request);
