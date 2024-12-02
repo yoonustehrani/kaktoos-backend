938e035773193df5cf60f08a5f574045943c40e7
yoonustehrani <yoonustehrani28@gmail.com>
2024-12-02 08:45:02 +0000

    new ticket data

--- resources/views/pdfs/ticket.blade.php
+++ resources/views/pdfs/ticket.blade.php
@@ -16,7 +16,7 @@
 </head>
 <body class="w-full min-w-[1280px] h-full p-1 bg-gray-800 font-roboto">
     @foreach ($passengers as $passenger)
-        <div class="w-full h-screen p-6 flex flex-col gap-6">
+        <div class="w-full h-fit p-6 flex flex-col gap-6">
             @foreach ($flights as $flight)
                 <div class="mx-auto w-full 2xl:w-4/5 flex h-[26rem] rounded-xl overflow-hidden bg-transparent">
                     {{-- Ticket --}}
@@ -85,8 +85,13 @@
                         </div>
                         <div class="w-full h-1/5 bg-white flex justify-around items-center gap-3 px-4">
                             <div class="flex gap-2 text-lg">
-                                <h1 class="font-bold">Name of Passenger</h1>
+                                <h1 class="font-bold">Passenger</h1>
                                 <p>{{ str($passenger->full_name)->upper($passenger->full_name) }}</p>
+                            </div>
+                            <div class="flex gap-2 text-lg">
+                                <h1 class="font-bold">Passenger Type</h1>
+                                <p>{{ ucfirst($passenger->gender) }}&comma;</p>
+                                <p>{{ ucfirst($passenger->type) }}</p>
                             </div>
                             <div class="flex gap-2 text-lg">
                                 <h1 class="font-bold">Flight</h1>
@@ -111,6 +116,11 @@
                                 <strong>Passenger</strong>
                                 <p class="bg-gray-200 grow w-auto px-2 py-1 italic rounded-md">{{ str($passenger->full_name)->upper() }}</p>
                             </div>
+                            <div class="flex gap-2 items-center w-full">
+                                <svg class="w-5 h-5" viewBox="-42 0 512 512.002" xmlns="http://www.w3.org/2000/svg"><path d="m210.351562 246.632812c33.882813 0 63.222657-12.152343 87.195313-36.128906 23.972656-23.972656 36.125-53.304687 36.125-87.191406 0-33.875-12.152344-63.210938-36.128906-87.191406-23.976563-23.96875-53.3125-36.121094-87.191407-36.121094-33.886718 0-63.21875 12.152344-87.191406 36.125s-36.128906 53.308594-36.128906 87.1875c0 33.886719 12.15625 63.222656 36.132812 87.195312 23.976563 23.96875 53.3125 36.125 87.1875 36.125zm0 0"/><path d="m426.128906 393.703125c-.691406-9.976563-2.089844-20.859375-4.148437-32.351563-2.078125-11.578124-4.753907-22.523437-7.957031-32.527343-3.308594-10.339844-7.808594-20.550781-13.371094-30.335938-5.773438-10.15625-12.554688-19-20.164063-26.277343-7.957031-7.613282-17.699219-13.734376-28.964843-18.199219-11.226563-4.441407-23.667969-6.691407-36.976563-6.691407-5.226563 0-10.28125 2.144532-20.042969 8.5-6.007812 3.917969-13.035156 8.449219-20.878906 13.460938-6.707031 4.273438-15.792969 8.277344-27.015625 11.902344-10.949219 3.542968-22.066406 5.339844-33.039063 5.339844-10.972656 0-22.085937-1.796876-33.046874-5.339844-11.210938-3.621094-20.296876-7.625-26.996094-11.898438-7.769532-4.964844-14.800782-9.496094-20.898438-13.46875-9.75-6.355468-14.808594-8.5-20.035156-8.5-13.3125 0-25.75 2.253906-36.972656 6.699219-11.257813 4.457031-21.003906 10.578125-28.96875 18.199219-7.605469 7.28125-14.390625 16.121094-20.15625 26.273437-5.558594 9.785157-10.058594 19.992188-13.371094 30.339844-3.199219 10.003906-5.875 20.945313-7.953125 32.523437-2.058594 11.476563-3.457031 22.363282-4.148437 32.363282-.679688 9.796875-1.023438 19.964844-1.023438 30.234375 0 26.726562 8.496094 48.363281 25.25 64.320312 16.546875 15.746094 38.441406 23.734375 65.066406 23.734375h246.53125c26.625 0 48.511719-7.984375 65.0625-23.734375 16.757813-15.945312 25.253906-37.585937 25.253906-64.324219-.003906-10.316406-.351562-20.492187-1.035156-30.242187zm0 0"/></svg>
+                                <strong>Passenger Type</strong>
+                                <p class="bg-gray-200 grow w-auto px-2 py-1 italic rounded-md">{{ ucfirst($passenger->gender) }}&comma;&nbsp;{{ ucfirst($passenger->type) }}</p>
+                            </div>
                             <div class="flex flex-wrap gap-2 items-center w-full">
                                 <svg class="w-5 h-5" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 512 512" style="enable-background:new 0 0 512 512;" xml:space="preserve"><g><g><path d="M256,0C161.896,0,85.333,76.563,85.333,170.667c0,28.25,7.063,56.26,20.49,81.104L246.667,506.5 c1.875,3.396,5.448,5.5,9.333,5.5s7.458-2.104,9.333-5.5l140.896-254.813c13.375-24.76,20.438-52.771,20.438-81.021 C426.667,76.563,350.104,0,256,0z M256,256c-47.052,0-85.333-38.281-85.333-85.333c0-47.052,38.281-85.333,85.333-85.333 s85.333,38.281,85.333,85.333C341.333,217.719,303.052,256,256,256z"/></g></g></svg>
                                 <strong>From</strong>
@@ -133,9 +143,9 @@
                                 <strong class="bg-[#173F28] inline-block text-gray-100 px-2">Flight</strong>
                                 <br><span class="font-mono">{{ $flight->flight_number }}</span>
                             </p>
-                            <p class="text-5xl font-bold">{{ $flight->departure_airport_code }}</p>
+                            <p class="text-4xl font-bold">{{ $flight->departure_airport_code }}</p>
                             <svg class="w-12 h-12" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 510 510" style="enable-background:new 0 0 510 510;" xml:space="preserve"><g><g><path d="M510,255c0-20.4-17.85-38.25-38.25-38.25H331.5L204,12.75h-51l63.75,204H76.5l-38.25-51H0L25.5,255L0,344.25h38.25 l38.25-51h140.25l-63.75,204h51l127.5-204h140.25C492.15,293.25,510,275.4,510,255z"/></g></g></svg>
-                            <p class="text-5xl font-bold">{{ $flight->arrival_airport_code }}</p>
+                            <p class="text-4xl font-bold">{{ $flight->arrival_airport_code }}</p>
                         </div>
                         <div class="w-full flex items-center justify-center">
                             <img class="h-16 w-auto" src="{{ asset('images/logo.png') }}" alt="">
--- routes/web.php
+++ routes/web.php
@@ -1,13 +1,62 @@
 <?php
 
+use App\Events\OrderPaid;
 use App\Http\Controllers\OrderController;
 use App\Http\Controllers\TicketController;
+use App\Jobs\OrderTransactionPaid;
+use App\Models\AirBooking;
+use App\Models\Order;
+use App\Models\Transaction;
+use App\Parto\Enums\HotelQueueStatus;
+use App\Payment\PaymentGateway;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Response;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Str;
+
+
+Route::get('/ticket', function() {
+    $airBooking = AirBooking::latest()->first();
+    $airBooking->load(['passengers.tickets', 'flights' => function($query) {
+        $query->with(['arrival_airport.country', 'departure_airport.country', 'marketing_airline', 'operating_airline']);
+    }]);
+    $airBooking->passengers->append('fullname')->makeHidden(['first_name', 'middle_name', 'last_name', 'title']);
+    $view = view('pdfs.ticket')
+        ->with('passengers', $airBooking->passengers)
+        ->with('flights', $airBooking->flights);
+    return $view;
+    // return $view->render();
+    // $response = Http::post('http://pdfrenderer:8082/render', [
+    //     'html' => $view->render(), // Render a Blade view
+    // ]);
+    // $pdf = $response->body();
+    return response($pdf, 200, [
+        'Content-Type' => 'application/pdf'
+    ]);
+});
+
+Route::get('/ticket/data', function() {
+    $airBooking = AirBooking::latest()->first();
+    $airBooking->load(['passengers.tickets', 'flights' => function($query) {
+        $query->with(['arrival_airport.country', 'departure_airport.country', 'marketing_airline', 'operating_airline']);
+    }]);
+    $airBooking->passengers->append('fullname')->makeHidden(['first_name', 'middle_name', 'last_name', 'title']);
+    return $airBooking;
+});
 
 Route::middleware('auth:sanctum')->group(function() {
     Route::get('orders/{order}/pay', [OrderController::class, 'pay'])->name('orders.pay');
     Route::get('air/bookings/{airBooking}/ticket', [TicketController::class, 'index'])->name('bookings.air.tickets.index');
     // Route::get('hotel/bookings/{hotelBooking}/voucher');
+});
+
+Route::get('/trx', function() {
+    $trx = Transaction::latest()->first();
+    $trx->order->user->increaseCredit($trx->amount);
+    OrderTransactionPaid::dispatch($trx);
 });
 
 Route::view('/', 'welcome');
