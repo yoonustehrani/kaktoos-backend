2a15158db5ec438fcf16da109a49f204b942ed41
yoonustehrani <yoonustehrani28@gmail.com>
2025-04-27 18:59:21 +0330

    default language for ticket file

--- resources/views/pdfs/ticket.blade.php
+++ resources/views/pdfs/ticket.blade.php
@@ -1,8 +1,8 @@
 @php
     if (
         request()->query('lang') == 'fa'
-        // ||
-        // ($booking->origin_airport->country_code  == 'IR' && $booking->destination_airport->country_code == 'IR')
+        ||
+        ($booking->origin_airport->country_code  == 'IR' && $booking->destination_airport->country_code == 'IR')
     ) {
         App::setLocale('fa');
     } else {
@@ -16,7 +16,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>بلیط پرواز خارجی</title>
+    <title>بلیط پرواز {{ App::getLocale() == "fa" ? 'داخلی' : 'خارجی' }}</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">
